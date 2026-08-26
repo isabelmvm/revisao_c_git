16280fd5f36aee4cca39f5238e71bfbe9b65c133
isabelmvm <isabelmelissa15@gmail.com>
2026-08-26 17:05:34 -0300

    Quinto exercicio concluido

---
+++ exerc_5/exerc_5.c
@@ -0,0 +1,4 @@
+// ​a) Vetor: Em C, é uma estrutura de dados que armazena uma sequência de elementos do mesmo tipo em posições contíguas de memória. 
+// É acessado via índices que iniciam em 0.
+// ​b) String: É tratada como um vetor de caracteres (char). 
+// A principal diferença é que strings em C devem ser finalizadas obrigatoriamente com o caractere nulo '\0' para indicar o fim do texto.
