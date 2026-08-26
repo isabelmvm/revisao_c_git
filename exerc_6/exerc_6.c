6b486d1112eb0c79794852356c20a816ef8c1e9e
isabelmvm <isabelmelissa15@gmail.com>
2026-08-26 17:16:12 -0300

    Sexto exercicio concluido

---
+++ exerc_6/exerc_6.c
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+// a) Saída do programa: O while imprimirá de while: 0 até while: 9. Quando o bloco terminar, i valerá 10. 
+// O bloco do..while será executado uma vez, imprimindo do: 10 e incrementando i para 11. Como 11 < 10 é falso, o loop é encerrado.  
+// ​b) Diferença while vs do..while: O while verifica a condição antes de executar o bloco de código. 
+// O do..while executa o bloco primeiro, e só depois verifica a condição, garantindo que o código rode pelo menos uma vez.  
+
+
+int main()
+{
+    int i;
+
+    i = 0;
+
+    // while verifica primeiro, depois executa
+    while (i < 10)
+    {
+        printf("while: %d\n", i);
+        i++;
+    }
+
+    // do executa, depois verifica
+    do
+    {
+        printf("do:%d\n", i);
+        i++;
+    } while (i < 10);
+
+    return 0;
+}
