4c27b11fa2ce2f1cf931324e149364e8a41c821e
isabelmvm <isabelmelissa15@gmail.com>
2026-08-26 18:14:45 -0300

    Decimo primeiro exercicio concluido

---
+++ exerc_11/exerc_11.c
@@ -0,0 +1,16 @@
+// 11. Estruturas de Dados Pilha, Fila e Lista
+
+// ​a) Diferença Pilha vs Fila: Pilha (Stack) segue o modelo LIFO (Último a Entrar, Primeiro a Sair). 
+// Fila (Queue) segue o modelo FIFO (Primeiro a Entrar, Primeiro a Sair).
+
+// ​b) Vantagens/Desvantagens da Fila: Vantagem: Processamento justo e em ordem (como senhas num banco). 
+// Desvantagem: Acesso direto a elementos do meio é complexo.
+
+// ​c) Vantagens/Desvantagens da Pilha: Vantagem: Perfeita para rastrear estados e histórico (reversão fácil). 
+// Desvantagem: Os elementos mais antigos ficam inacessíveis até que os mais novos sejam removidos.
+
+// ​d) Aplicações: Fila: Gerenciamento de impressão, buffer de teclado. 
+// Pilha: Função de "Desfazer" (Ctrl+Z) em editores, execução de funções na memória (Call Stack).
+
+// ​e) Diferença para a Lista: As listas encadeadas permitem inserção, remoção e visualização de elementos em qualquer posição 
+// (início, meio ou fim), enquanto pilhas e filas restringem o acesso apenas às suas extremidades (topo ou cauda/frente).
