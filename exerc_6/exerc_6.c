abf5487842038a675942d20ece22e8186bca9f54
isabelmvm <isabelmelissa15@gmail.com>
2026-08-26 17:18:31 -0300

    Sexto exercicio concluido  atualizado

--- exerc_6/exerc_6.c
+++ exerc_6/exerc_6.c
@@ -3,7 +3,8 @@
 // O bloco do..while será executado uma vez, imprimindo do: 10 e incrementando i para 11. Como 11 < 10 é falso, o loop é encerrado.  
 // ​b) Diferença while vs do..while: O while verifica a condição antes de executar o bloco de código. 
 // O do..while executa o bloco primeiro, e só depois verifica a condição, garantindo que o código rode pelo menos uma vez.  
-
+// ​d) Diferença while e for: O while é ideal quando o número de iterações é desconhecido. 
+// O for é recomendado quando se sabe a quantidade de iterações, pois agrupa a inicialização, a condição e o incremento na mesma linha.
 
 int main()
 {
